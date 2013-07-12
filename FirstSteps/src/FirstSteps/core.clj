db04f5c754e61083d64606bb1e84d0f50795dffb
jakub latttenberk <lattenberk.jakub@gmail.com>
2013-07-12 13:34:10 +0200

    test

---
+++ FirstSteps/src/FirstSteps/core.clj
@@ -0,0 +1,16 @@
+(ns FirstSteps.core)
+
+(defn foo "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!")
+)
+
+(defn multiply
+    ([x]
+     (* x x))
+    ([x y]
+     (* x y))
+    ([x y z] (* x y z))
+)
+
+(defn hello [x](println x "muhehe"))
